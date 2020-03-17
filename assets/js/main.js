bfd643c394f763fce91de249585d65c289b2d2f5
bedimcode <61248995+bedimcode@users.noreply.github.com>
2020-03-17 08:39:12 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/*MENU SHOW*/ 
+const showMenu = (toggleId,navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
