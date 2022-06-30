9ca895927e787d4196766669fe5e6a8d326929a7
Marlon Pilco (Bedimcode) <bedimcode@gmail.com>
2022-06-29 19:48:36 -0500

    Updating the project Added changes to index.html Variables in root are modified in the SCSS file Added GSAP animation in main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*MENU SHOW*/ 
+/* MENU SHOW */ 
 const showMenu = (toggleId,navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -11,3 +11,19 @@ const showMenu = (toggleId,navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
+/* GSAP ANIMATION */
+/*HOME PRIMARY*/
+TweenMax.to(".home__primary", 2, {
+    width: "100%",
+    ease: Expo.easeInOut
+})
+gsap.from('.home__title', {opacity: 0, duration: 2, delay: 1.5, y: 100})
+gsap.from('.home__img img', {opacity: 0, duration: 2, delay:1.5, y: -100})
+
+/*HOME SECONDARY*/
+TweenMax.to(".home__secondary", 2.5, {
+    width: "100%",
+    ease: Expo.easeInOut
+})
+gsap.from('.home__scroll', {opacity: 0, duration: 3, x: -100})
+gsap.from('.home__year', {opacity: -1, duration: 1.5, delay: 1.5, x: 100})
